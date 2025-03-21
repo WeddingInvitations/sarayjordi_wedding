28c83eef66a9b0fcd103e77be8b4ab482665c940
O001830 <fernando.aguilar.santos@bbva.com>
2025-03-21 11:05:13 +0100

    add password

--- www/js/script.js
+++ www/js/script.js
@@ -1,3 +1,5 @@
+// js/welcome.js
+
 // Función para autenticarse
 function authenticate(event) {
   event.preventDefault();
@@ -6,17 +8,16 @@ function authenticate(event) {
 
   // Verificar las credenciales
   if (password === 'Bodorrio2025') {
-    // Credenciales válidas, mostrar el contenido
-    document.getElementById('login-container').style.display = 'none';
-    document.getElementById('content-container').style.display = 'block';
-    // Almacenar en sessionStorage que el usuario ha iniciado sesión
-    sessionStorage.setItem('isLoggedIn', 'true');
+      // Credenciales válidas, mostrar el contenido
+      document.getElementById('login-container').style.display = 'none';
+      document.getElementById('content-container').style.display = 'block';
+      // Almacenar en sessionStorage que el usuario ha iniciado sesión
+      sessionStorage.setItem('isLoggedIn', 'true');
   } else {
-    // Credenciales incorrectas, mostrar un mensaje de error
-    alert('Credenciales incorrectas. Por favor, inténtalo de nuevo.');
+      // Credenciales incorrectas, mostrar un mensaje de error
+      alert('Credenciales incorrectas. Por favor, inténtalo de nuevo.');
   }
 }
-
 
 // Evento "load" inicial
 window.addEventListener("load", function () {
@@ -24,24 +25,23 @@ window.addEventListener("load", function () {
   var isLoggedIn = sessionStorage.getItem('isLoggedIn');
 
   if (isLoggedIn === 'true') {
-    // El usuario ha iniciado sesión previamente
-    ocultarContenedorDeInicioDeSesion();
-    mostrarContenido(); // Mostrar el contenido directamente
+      // El usuario ha iniciado sesión previamente
+      ocultarContenedorDeInicioDeSesion();
+      mostrarContenido(); // Mostrar el contenido directamente
   }
 
   // Agregar el event listener al formulario de inicio de sesión *aquí*, después de que el DOM esté cargado
   const loginForm = document.getElementById('login-form');
   if (loginForm) {
-    loginForm.addEventListener('submit', authenticate);
+      loginForm.addEventListener('submit', authenticate);
   }
 });
-
 
 // Función para ocultar el contenedor de inicio de sesión
 function ocultarContenedorDeInicioDeSesion() {
   var loginContainer = document.getElementById('login-container');
   if (loginContainer) {
-    loginContainer.style.display = 'none';
+      loginContainer.style.display = 'none';
   }
 }
 
@@ -49,6 +49,6 @@ function ocultarContenedorDeInicioDeSesion() {
 function mostrarContenido() {
   var contentContainer = document.getElementById('content-container');
   if (contentContainer) {
-    contentContainer.style.display = 'block';
+      contentContainer.style.display = 'block';
   }
 }
